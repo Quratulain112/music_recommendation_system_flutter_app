0250be2dbc5df6541e25233ce7345b4fab7c27b5
Muhammed Zohaib Anwer <zohaibanwer2002@gmail.com>
2024-11-04 21:59:24 +0500

    Updated Discover Page
    add list view in code . made code more efficient

--- lib/pages/discover_page.dart
+++ lib/pages/discover_page.dart
@@ -3,42 +3,47 @@ import 'package:music_app/widgets/list_item.dart';
 import 'package:music_app/widgets/search_bar.dart';
 
 class DiscoverPage extends StatelessWidget {
-  const DiscoverPage({super.key});
+  DiscoverPage({super.key});
+
+  final List<dynamic> songs = [
+    ["SONGS NAME", "ARTIST"],
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: 500,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage("assets/glitter_one.jfif"))),
-        child: SingleChildScrollView(
-          physics: BouncingScrollPhysics(),
-          child: Padding(
-            padding: EdgeInsets.only(top: 75),
-            child: Column(
-              children: [
-                Text(
-                  "What's on your mind?",
-                  style: TextStyle(
-                      color: const Color.fromARGB(255, 228, 216, 230),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage("assets/glitter_one.jfif"),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20, left: 15, right: 15),
+          child: Column(
+            children: [
+              const Text(
+                "What's on your mind?",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 228, 216, 230),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 15),
+                child: MySearchBar(),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: songs.length,
+                  itemBuilder: (context, index) {
+                    return MyListItem(
+                        song_name: songs[index][0],
+                        artist_name: songs[index][1]);
+                  },
                 ),
-                Padding(
-                  padding: EdgeInsets.only(top: 15),
-                  child: MySearchBar(),
-                ),
-                MyListItem(song_name: "Song one", artist_name: "artist name"),
-                MyListItem(song_name: "Song two", artist_name: "artist name"),
-                MyListItem(song_name: "Song three", artist_name: "artist name"),
-                MyListItem(song_name: "Song four", artist_name: "artist name"),
-                MyListItem(song_name: "Song five", artist_name: "artist name"),
-                MyListItem(song_name: "Song six", artist_name: "artist name"),
-                MyListItem(song_name: "Song seven", artist_name: "artist name"),
-              ],
-            ),
+              ),
+            ],
           ),
         ));
   }

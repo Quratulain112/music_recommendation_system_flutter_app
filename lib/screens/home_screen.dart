9b18960120802903a8fb1cdd7cd379ece82bf2b7
Quratulain112 <167783489+Quratulain112@users.noreply.github.com>
2024-11-05 01:14:42 +0500

    added drawer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,6 +9,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeState extends State<HomeScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
   List<Widget> _pages = [
     DiscoverPage(),
     MusicPage(),
@@ -24,11 +25,87 @@ class _HomeState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      key: _scaffoldKey,
+      drawer: Drawer(
+        backgroundColor: const Color.fromARGB(153, 39, 16, 60),
+        width: 240,
+        child: Padding(
+          padding: EdgeInsets.only(left: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                radius: 45,
+                backgroundColor: Colors.pink,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10, bottom: 8),
+                child: Text(
+                  "Hi!!",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              Text(
+                "User \nName",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                    color: Colors.white),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.home,
+                  color: Colors.white70,
+                ),
+                title: Text(
+                  "Home",
+                  style: TextStyle(color: Colors.white70),
+                ),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.person,
+                  color: Colors.white70,
+                ),
+                title: Text(
+                  "Artist",
+                  style: TextStyle(color: Colors.white70),
+                ),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.favorite,
+                  color: Colors.white70,
+                ),
+                title: Text(
+                  "Favorite",
+                  style: TextStyle(color: Colors.white70),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Logout",
+                    style: TextStyle(color: Colors.white70),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 40, 4, 46),
-        leading: CircleAvatar(
-          backgroundColor: Colors.pink,
+        leading: GestureDetector(
+          onTap: () {
+            _scaffoldKey.currentState?.openDrawer();
+          },
+          child: CircleAvatar(
+            backgroundColor: Colors.pink,
+          ),
         ),
         title: Text(
           "Discover",

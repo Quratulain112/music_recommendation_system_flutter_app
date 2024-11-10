f677ba81586f76199be0c5af35126a84e623f585
Quratulain112 <167783489+Quratulain112@users.noreply.github.com>
2024-11-10 22:10:02 +0500

    Updated Searchbar

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -5,57 +5,38 @@ class MySearchBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return SizedBox(
-    //   height: 35,
-    //   width: 250,
-    //   child: TextField(
-    //     style: TextStyle(color: Colors.white),
-    //     decoration: InputDecoration(
-    //       hintText: 'Search...',
-    //       hintStyle: TextStyle(color: Colors.white54),
-    //       border: OutlineInputBorder(
-    //         borderRadius: BorderRadius.circular(8.0),
-    //       ),
-    //       suffixIcon: Icon(Icons.search, color: Colors.white),
-    //       filled: true,
-    //       fillColor: Colors.black26, // Optional: to give a background color
-    //     ),
-    //   ),
-    // );
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
-        alignment: Alignment.center,
-        child: TextField(
-          decoration: InputDecoration(
-            filled: true,
-            fillColor:
-                Colors.white.withOpacity(0.06), // Semi-transparent background
-            prefixIcon: const Icon(
-              Icons.search,
-              size: 30,
-              color: Colors.white70,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25),
+          border: Border.all(color: Colors.white70, width: 2),
+          color: Colors.white.withOpacity(0.06),
+        ),
+        child: Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(12),
+              child: Icon(Icons.search, color: Colors.white),
             ),
-            hintText: "Search",
-            hintStyle: const TextStyle(fontSize: 16, color: Colors.white70),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(25),
-              borderSide: BorderSide(
-                color: const Color.fromARGB(204, 192, 181, 181)
-                    .withOpacity(0.5), // Semi-transparent border
-                width: 2,
+            Expanded(
+              child: TextField(
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Enter prompt",
+                  hintStyle: TextStyle(color: Colors.white70),
+                ),
               ),
             ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(25),
-              borderSide: BorderSide(
-                color: Colors.white10
-                    .withOpacity(0.8), // Slightly more opaque when focused
-                width: 2,
+            Padding(
+              padding: EdgeInsets.only(right: 10),
+              child: Icon(
+                Icons.send,
+                color: Colors.white,
               ),
             ),
-          ),
-          style: const TextStyle(color: Colors.white30), // Text color
+          ],
         ),
       ),
     );
